6e9bdbf6eba956336327d4fe2a14ac1173f2daa7
zerosial <zerosial@gmail.com>
2022-06-11 02:55:34 +0900

    📰New: CoinTracket 제작중

--- practice-react/src/index.tsx
+++ practice-react/src/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { theme } from "./theme";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
